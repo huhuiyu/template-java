b5e7615840b40eee470c3e194cdb9c29a13d7aa7
huhuiyu <1069306849@qq.com>
2023-06-25 08:20:19 +0800

    项目初始化

---
+++ push.ps1
@@ -0,0 +1,7 @@
+# git@github.com:huhuiyu/template-java.git
+# git remote add jihulab git@jihulab.com:DarkKnight/template-javaweb.git
+# git remote add gitee git@jihulab.com:DarkKnight/template-java.git
+git pull
+git push
+git push jihulab
+git push gitee
