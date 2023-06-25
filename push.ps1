599e8141ec426c5b913a735f4d45adeec9b04465
huhuiyu <1069306849@qq.com>
2023-06-25 08:21:16 +0800

    推送地址错误

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 # git@github.com:huhuiyu/template-java.git
-# git remote add jihulab git@jihulab.com:DarkKnight/template-javaweb.git
+# git remote add jihulab git@jihulab.com:DarkKnight/template-java.git
 # git remote add gitee git@jihulab.com:DarkKnight/template-java.git
 git pull
 git push
