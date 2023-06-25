9a2fd463f86cba9357c1330d74df93161ed4ee87
huhuiyu <1069306849@qq.com>
2023-06-25 08:22:44 +0800

    gitee地址修改

--- push.ps1
+++ push.ps1
@@ -1,6 +1,6 @@
 # git@github.com:huhuiyu/template-java.git
 # git remote add jihulab git@jihulab.com:DarkKnight/template-java.git
-# git remote add gitee git@jihulab.com:DarkKnight/template-java.git
+# git remote add gitee git@gitee.com:huhuiyu/template-java.git
 git pull
 git push
 git push jihulab
